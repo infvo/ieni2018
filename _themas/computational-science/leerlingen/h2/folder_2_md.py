d247b47c18848fffa085cfb1fa16884e252a2321
Vincent Velthuizen <mail@vincentvelthuizen.com>
2019-03-26 13:24:42 +0100

    extra stap in het pad is wel nodig

--- _themas/computational-science/leerlingen/h2/folder_2_md.py
+++ _themas/computational-science/leerlingen/h2/folder_2_md.py
@@ -8,12 +8,12 @@ def folder_2_md(path, output, prefix=None):
         base_name, extension = os.path.splitext(file_name)
         if prefix is None:
             prefix = extension
-        file_location = relative_path + file_name
+        file_location = relative_path + path + file_name
         output.write("* %s [%s](%s)\n" % (prefix, base_name, file_location))
 
 
 with open("files.md", 'w+') as output:
-    folder_2_md("./NetLogo_opdrachten/", output)
+    folder_2_md("NetLogo_opdrachten/", output)
     output.write("\n")
-    folder_2_md("./video/", output, "VID")
+    folder_2_md("video/", output, "VID")
  
